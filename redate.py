7370752a48ef1734ca930c025611e6d31044410e
Serhiy Sambros <serhiy.sambros@gmail.com>
2012-12-27 16:59:15 -0800

    Main files commit

---
+++ redate.py
@@ -0,0 +1,16 @@
+
+
+class ReDate():
+
+    def __init__(date_start, date_end):
+        self.date_start = date_start
+        self.date_end = date.end
+
+    def get():
+        return None
+
+    def get(date_format):
+        return None
+
+
+
---
+++ redate_test.py
@@ -0,0 +1,16 @@
+import random
+import unittest
+
+class TestReDateGenerator(unittest.TestCase):
+
+    
+    def test_default_format(self):
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
